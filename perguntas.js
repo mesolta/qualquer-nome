e6308dab5c322cc4ee8929f0589f978a7dbaaa67
mesolta <lipesebastiao@gmail.com>
2024-11-26 15:24:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    '1',
+    ' Qual é o carro de luxo mais caro do mundo?',
+    'O Rolls-Royce Boat Tail é considerado um dos carros de luxo mais caros do mundo, custando cerca de 28 milhões de dólares'
+)
+
+criaCartao(
+    '2',
+    'Quais marcas são mais conhecidas por fabricar carros de luxo?',
+    'Marcas como Rolls-Royce, Bentley, Ferrari, Lamborghini, e Bugatti são algumas das mais renomadas no segmento de luxo.'
+)
+
+criaCartao(
+    '3',
+    'O que diferencia um carro de luxo de um carro comum?',
+    'Carros de luxo se destacam por materiais de alta qualidade, tecnologias avançadas, motores potentes, e exclusividade no design.'
+)
+
+criaCartao(
+    '4',
+    'Carros de luxo são econômicos?',
+    'Geralmente, carros de luxo não são focados em economia de combustível, mas em desempenho e conforto. No entanto, alguns modelos híbridos ou elétricos estão mudando isso'
+)
